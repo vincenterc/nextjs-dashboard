349c8fe2e70fed95dfe90535869f98f2eb410e42
vincenter <vincenterr@yahoo.com.tw>
2024-07-02 14:28:14 +0800

    Update

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,1 @@
-const styleguide = require('@vercel/style-guide/prettier');
-
-module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
+module.exports = { singleQuote: true };
